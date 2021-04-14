bd8a56fa80608ae7506d7281da9f830b56780d99
DESKTOP-OKQQ9TH\Cong <dangcong1810@gmail.com>
2021-04-14 15:43:43 +0700

    xong

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
+import Input from "./components/Input";
+import TodoItem from "./components/TodoItem";
+import { useSelector } from "react-redux";
+import { selectTodoList } from "./features/todoSlice";
 
 function App() {
+  const todoList = useSelector(selectTodoList);
   return (
-    <div className="App">
+    <div className="app">
       {/* todo list */}
-      {/* done */}
-      {/* input */}
-      <h1>hello</h1>
+      <div className="app__container">
+        <div className="app__todoContainer">
+          {todoList.map((item) => (
+            <TodoItem name={item.item} done={item.done} id={item.id} />
+          ))}
+        </div>
+        <Input />
+      </div>
     </div>
   );
 }
